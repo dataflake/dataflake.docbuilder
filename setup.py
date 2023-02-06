1850274139a17842109562d44617aab264f7911f
Jens Vagelpohl <jens@plyp.com>
2023-02-06 16:38:35 +0100

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 
 setup(name='dataflake.docbuilder',
-      version='2.2',
+      version='2.3.dev0',
       description='Automated Sphinx documentation builder',
       long_description=read('README.rst'),
       classifiers=[
